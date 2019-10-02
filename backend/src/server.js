1702e1f0ba23bb0c3a3f6250127729d427357041
¨Cristiano <cristiano.so@hotmail.com>
2019-10-02 13:11:13 -0300

    User,SessionController

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://cristiano:cristiano@omnistack-9g5dc.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // GET, POST, PUT, DELETE
 
