d352a39a1786a2f647a86885aa84f6ea565a7bda
¨Cristiano <cristiano.so@hotmail.com>
2019-10-02 11:25:49 -0300

    teste

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,14 @@ const app = express();
 
 // GET, POST, PUT, DELETE
 
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição) 
+
+app.use(express.json());
+
 app.post('/users',(req, res)=>{
-    return res.json({message: "Hello OmniStack"});
+    return res.json(req.body);
 })
 
 app.listen(3333);
