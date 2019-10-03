8d15ddb5fe8f39d1d8cb6724c234c5bc7c8732fa
¨Cristiano <cristiano.so@hotmail.com>
2019-10-03 16:52:22 -0300

    login, dashboard, new, routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://cristiano:cristiano@omnistack-9g5dc.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição) 
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
  
