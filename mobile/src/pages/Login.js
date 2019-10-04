d0b1bf9ac6bc40db85ab55c59ae5247f3618adbe
Cristiano Oliveira <cristiano.so@hotmail.com>
2019-10-04 20:15:55 -0300

    services, login

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { View, Image, Text, TextInput, TouchableOpacity , StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage , Image, KeyboardAvoidingView, Platform , Text, TextInput, TouchableOpacity , StyleSheet } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login(){
+export default function Login({ navigation }){
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user =>{
+            if(user){
+                navigation.navigate('List');
+            }
+        })
+    }, []);
+
+    async function handleSubmit(){
+        //email, techs
+        const response = await api.post('/sessions',{
+            email
+        })
+
+        const { _id } = response.data;
+        
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
     return (
-        <View style = {styles.container}>
+        <KeyboardAvoidingView  enabled={Platform.os == 'ios'} behavior="padding" style = {styles.container}>
             <Image source={logo}/>
 
             <View style={styles.form}>
@@ -17,6 +44,8 @@ export default function Login(){
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
 
                 <Text style= {styles.label}>TECNOLOGIAS *</Text>
@@ -26,13 +55,15 @@ export default function Login(){
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                     <Text style={styles.buttonText}>Encontrar spots</Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
